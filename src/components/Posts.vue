0215ba6797720e9a390b1984c9950c939ddfac72
Francie Jiang <i@francie.cc>
2022-08-06 09:47:07 +0800

    Changed styles

--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -13,7 +13,7 @@
 </script>
 
 <template>
-    <div class="flex rounded-lg border-2 border-gray-700 dark:border-slate-800 mt-2 w-full h-fit p-2 lg:p-3 flex-col">
+    <div class="flex rounded-sm border-2 border-gray-700 dark:border-slate-800 mt-2 w-full h-fit p-2 lg:p-3 flex-col shadow-md">
         <div class="text-base lg:text-sm font-archivo-narrow">
             <i class="opacity-50 fa-solid fa-clock"></i>
             {{ postDate }}
